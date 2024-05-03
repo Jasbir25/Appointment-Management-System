931f53e64c509a7e34587eafe6d35db0aeb5eca2
Jasbir Singh <101258927+Jasbir25@users.noreply.github.com>
2024-05-04 01:40:26 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def modify_appointment_data(conn, appointment):
-#     cur = conn.cursor()
-#     cur.execute("""
-#         UPDATE appointment
-#         SET appointment_date =?, appointment_time =?, patient_name =?, reason =?
-#         WHERE id =?
-#     """, appointment)
-#     conn.commit()
-#     return True
